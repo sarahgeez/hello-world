9b71280ce87b71bd4777123cc9bb954fec49e338
sarahgeez <74019269+sarahgeez@users.noreply.github.com>
2021-01-15 14:53:10 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.No)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     led.plot(2, 2)
 })
 input.onGesture(Gesture.Shake, function () {
@@ -75,7 +82,4 @@ input.onGesture(Gesture.Shake, function () {
         . . . . .
         . . . . .
         `)
-})
-basic.forever(function () {
-	
 })
